37a0171b3b30fb5b7001edb77d13bd433c77c1c5
DanjelKyle <107757601+DanjelKyle@users.noreply.github.com>
2024-01-25 13:11:50 +0100

    ADD naME

--- events_fe/src/App.js
+++ events_fe/src/App.js
@@ -44,7 +44,7 @@ const router = createBrowserRouter([
 ])
 
 function App() {
-    //Danjel
+    //Danjel A
     return <RouterProvider router={router}/>;
     // return <YourComponent/>
     // return <PhotoList/>
